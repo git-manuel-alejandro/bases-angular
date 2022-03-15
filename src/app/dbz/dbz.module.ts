448415734b330ff6e76195fa844929f891c72aa1
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-03-15 10:02:59 -0300

    commit

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+
 import { MainPageComponent } from './main-page/main-page.component';
+import { PersonajesComponent } from './personajes/personajes.component';
 
 
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    PersonajesComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
   ],
   exports:[
     MainPageComponent
--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Personaje } from '../interface/dbz.interface';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent {
 
-  constructor() { }
+  personajes : Personaje[] = [
+    {
+      nombre : 'Goku',
+      poder: 15000
+    },
+    {
+      nombre : 'Gohan',
+      poder: 9000
+    },
+    {
+      nombre : 'Vegeta',
+      poder: 12000
+    },
+  ]
 
-  ngOnInit(): void {
+  nuevo:Personaje = {
+    nombre : '',
+    poder : 0
+  }
+
+  agregar(){
+    this.personajes.push(this.nuevo)
+    this.nuevo = {
+      nombre  : '',
+      poder : 0
+    }
+    return
+    
   }
 
 }
